0b1817ac246e4e511aa586055be4584902b41060
NeekunjGupta <75844043+NeekunjGupta@users.noreply.github.com>
2021-05-12 20:47:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function preload() {
+   
+}
+
+function setup(){
+    var canvas = createCanvas(1200,1200);
+   
+}
+
+function draw(){
+    background(0);
+    textSize(35);
+    fill("white");
+    text("Ingredients:",150,100);
+    text("_________",150,105);
+    text("1. Carrots",150,150);
+    text("2. Milk",150,200);
+    text("3. Sugar",150,250);
+    text("Recipe:",150,300);
+    text("______",150,305);
+    text("Step 1 : Boil Milk.",150,350);
+    text("Step 2 : Put grated Carrots in in boiled Milk.",150,400);
+    text("Step 3 : Keep Stiring.",150,450);
+    text("Step 4 : Put Sugar in it.",150,500);
+    text("Step 5 : Carrot`s porridge",150,550);
+}
